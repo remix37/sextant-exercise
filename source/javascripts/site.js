71b574fcd448b86217e93f50083393f84cdc9da1
Rémy Rigault <r.rigault@me.com>
2018-07-02 15:29:09 +0200

    init carousel & add new sections

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,18 @@
 // This is where it all goes :)
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
