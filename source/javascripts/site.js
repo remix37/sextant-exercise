b36587f1bf1e506f14c054c73b0cbde6c10e237b
Rémy Rigault <r.rigault@me.com>
2018-07-03 19:17:42 +0200

    improve carousel design

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -6,6 +6,12 @@ $('.owl-carousel').owlCarousel({
     nav:true,
     autoplay: true,
     autoplayTimeout: 3000,
+    navText :[
+      "<img align='middle' alt='' src='/images/arrow-left-carousel.svg' style='margin-bottom: 1px;' width='25'>",
+      "<img align='middle' alt='' src='/images/arrow-right-carousel.svg' style='margin-bottom: 1px;' width='25'>"
+    ],
+    navContainerClass : "owl-nav visible-xs visible-sm",
+    dotClass: "owl-dot hidden-xs hidden-sm",
     responsive:{
         0:{
             items:1
